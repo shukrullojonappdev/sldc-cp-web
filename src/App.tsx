2b3171b23ba636e2d121abfe3df0a8d67f75d9a1
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-07-11 06:04:37 -0700

    repair

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
-import * as React from 'react'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
-import MainPage from './pages/MainPage'
-import AuthPage from './pages/AuthPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomeScreen from './screens/HomeScreen'
-import ApplicationsScreen from './screens/ApplicationsScreen'
-import WarehouseScreen from './screens/WarehouseScreen'
-import ClientsScreen from './screens/ClientsScreen'
-import AgentsScreen from './screens/AgentsScreen'
-import SettingsScreen from './screens/SettingsScreen'
-import CreateOrderScreen from './screens/CreateOrderScreen'
-import LeftoversScreen from './screens/LeftoversScreen'
-import AdmissionScreen from './screens/Admission'
+import MainPage from "./pages/MainPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ApplicationsScreen from "./screens/ApplicationsScreen";
+import WarehouseScreen from "./screens/WarehouseScreen";
+import ClientsScreen from "./screens/ClientsScreen";
+import AgentsScreen from "./screens/AgentsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import CreateOrderScreen from "./screens/CreateOrderScreen";
+import LeftoversScreen from "./screens/LeftoversScreen";
+import AdmissionScreen from "./screens/Admission";
 
 export default function App() {
   return (
-		<BrowserRouter>
-			<div className="App">
-				<Routes>
-					<Route path='/' element={<MainPage />}>
-						<Route index element={<HomeScreen />} />
-						<Route path='applications' element={<ApplicationsScreen />} />
-						<Route path='createOrder' element={<CreateOrderScreen />} />
-						<Route path='warehouse' element={<WarehouseScreen />} />
-						<Route path='leftovers' element={<LeftoversScreen/>} />
-						<Route path='admission' element={<AdmissionScreen />} />
-						<Route path='clients' element={<ClientsScreen />} />
-						<Route path='agents' element={<AgentsScreen />} />
-						<Route path='settings' element={<SettingsScreen />} />
-					</Route>
-				</Routes>
-			</div>
-		</BrowserRouter>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />}>
+            <Route index element={<HomeScreen />} />
+            <Route path="applications" element={<ApplicationsScreen />} />
+            <Route path="createOrder" element={<CreateOrderScreen />} />
+            <Route path="warehouse" element={<WarehouseScreen />} />
+            <Route path="leftovers" element={<LeftoversScreen />} />
+            <Route path="admission" element={<AdmissionScreen />} />
+            <Route path="clients" element={<ClientsScreen />} />
+            <Route path="agents" element={<AgentsScreen />} />
+            <Route path="settings" element={<SettingsScreen />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
